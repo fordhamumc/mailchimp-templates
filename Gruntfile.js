245ec178c363c52ca2ea0ba733c7fbf7824e2828
Michael Foley <michaeldfoley@gmail.com>
2016-01-27 14:13:54 -0500

    Updated gruntfile to handle jade templates and file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,314 +1,360 @@
-module.exports = function (grunt) {
-    'use strict';
-    
-    // List all files in the app directory.
-    var templates = grunt.file.expand({filter: 'isFile', cwd: 'app'},
-                                      ['*.html']);
+module.exports = function(grunt) {
+  'use strict';
 
-    // Make actual choices out of them that grunt-prompt can use.
-    var choices = templates.map(function (t) {
-        return { name: t.replace('.html','')};
-    });
-    
-    // Show elapsed time after tasks run
-    require('time-grunt')(grunt);
-    
-    // Load all Grunt tasks
-    require('load-grunt-tasks')(grunt);
-    
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  // List all files in the app directory.
+  var templates = grunt.file.expand({
+    filter: 'isFile',
+    cwd: 'app'
+  }, ['**/*.jade', '!_*/**/*.jade']);
 
-        /**
-         * Project Paths Configuration
-         * ===============================
-         */
-        paths: {
-            images: 'img',
-            src: 'app',
-            email: templates[0].replace('.html', ''),
-            dist: 'dist',
-            distDomain: '', 
-        		sender: {
-          		service: '',
-          		user: '',
-              pass: ''
-        		},
-        		recipients: {
-          		name: '',
-          		email: '',
-          		subject: ''
-        		}
-        },
+  // Make actual choices out of them that grunt-prompt can use.
+  var choices = templates.map(function(t) {
+    return {
+      name: t.substr(t.indexOf('/') + 1).replace('.jade', ''),
+      value: t.replace('.jade', '')
+    };
+  });
 
-        /**
-         * SCSS Compilation Tasks
-         * ===============================
-         */
-        compass: {
-            options: {
-                sassDir: '<%= paths.src %>/scss',
-                outputStyle: 'expanded',
-                httpImagesPath: '/img/'
-            },
-            dev: {
-                options: {
-                    cssDir: '<%= paths.src %>/css',
-                    imagesDir: '<%= paths.src %>/<%= paths.images %>',
-                    noLineComments: false
-                }
-            },
-            dist: {
-                options: {
-                    force: true,
-                    cssDir: '<%= paths.dist %>/<%= paths.email %>/css',
-                    imagesDir: '<%= paths.dist %>/<%= paths.email %>/<%= paths.images %>',
-                    noLineComments: true,
-                    assetCacheBuster: false,
-                    outputStyle: "compact"
-                }
-            }
-        },
+  // Show elapsed time after tasks run
+  require('time-grunt')(grunt);
 
-        /**
-         * Watch Task
-         * ===============================
-         */
-        watch: {
-            compass: {
-                files: ['<%= paths.src %>/scss/**/*.scss'],
-                tasks: ['compass:dev']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= paths.src %>/<%= paths.email %>.html',
-                    '<%= paths.src %>/css/{,*/}*.css',
-                    '<%= paths.src %>/<%= paths.images %>/{,*/}*.{png,jpg,jpeg,gif}'
-                ]
-            }
-        },
+  // Load all Grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-        /**
-         * Server Tasks
-         * ===============================
-         */
-        connect: {
-            options: {
-                open: true,
-                hostname: 'localhost',
-                port: 8000,
-                livereload: 35729
-            },
-            dev: {
-                options: {
-                    base: '<%= paths.src %>',
-                    open: {
-                      target: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/<%= paths.email %>.html'
-                    }
-                }
-            },
-            dist: {
-                options: {
-                    keepalive: true,
-                    livereload: false,
-                    base: '<%= paths.dist %>/<%= paths.email %>'
-                }
-            }
-        },
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        /**
-         * Cleanup Tasks
-         * ===============================
-         */
-        clean: {
-            dist: ['.tmp', '<%= paths.dist %>/<%= paths.email %>']
-        },
-        mkdir: {
-          dist: {
-            options: {
-              create: ['.tmp']
-            },
-          },
-        },
+    /**
+     * Project Paths Configuration
+     * ===============================
+     */
+    paths: {
+      images: 'img',
+      src: 'app',
+      directory: templates[0].substr(0, templates[0].indexOf('/')),
+      file: templates[0].substr(templates[0].indexOf('/') + 1).replace('.jade', ''),
+      tmp: '.tmp',
+      dist: 'dist',
+      distDomain: '',
+      sender: {
+        service: '',
+        user: '',
+        pass: ''
+      },
+      recipients: {
+        name: '',
+        email: '',
+        subject: ''
+      }
+    },
 
-        /**
-         * Images Optimization Tasks
-         * ===============================
-         */
-        img_find_and_copy: {
-          dist: {
-            options: {
-              cwd: '<%= paths.src %>'
-            },
-            files: {
-              '.tmp': ['<%= paths.src %>/css/**/*.css', '<%= paths.src %>/<%= paths.email %>.html']
-            }
-          }
-        }, 
-        
-        imagemin: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '.tmp/<%= paths.src %>',
-                    src: '<%= paths.images %>/{,*/}*.{png,jpg,jpeg,gif}',
-                    dest: '<%= paths.dist %>/<%= paths.email %>'
-                }]
-            }
-        },
+    /**
+     * Copy Tasks
+     * ===============================
+     */
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.src %>',
+          src: ['<%= paths.directory %>/<%= paths.images %>/**/*.{png,jpg,jpeg,gif}', '_components/**/*.{png,jpg,jpeg,gif}'],
+          dest: '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.images %>',
+          flatten: true
+        }]
+      }
+    },
 
-        /**
-         * Premailer Parser Tasks
-         * ===============================
-         */
-        premailer: {
-            options: {
-                baseUrl: '<%= paths.distDomain %>'
-            },
-            plain: {
-                options: {
-                    mode: 'txt'
-                },
-                src: '<%= paths.src %>/<%= paths.email %>.html',
-                dest: '<%= paths.dist %>/<%= paths.email %>/index.txt'
-            },
-            dist: {
-                src: '<%= paths.src %>/<%= paths.email %>.html',
-                dest: '<%= paths.dist %>/<%= paths.email %>/index.html'
-            }
-        },
-        
-         /**
-         * Replacement Tasks
-         * ===============================
-         */
-        replace: {
-          imageDir: {
-            src: '<%= paths.dist %>/<%= paths.email %>/index.html',
-            dest: '<%= paths.dist %>/<%= paths.email %>/index.html', 
-            replacements: [{
-              from: '/img/', 
-              to: '/'
-            },
-            {
-              from: 'margin', 
-              to: 'Margin'
-            }]
-          }
-        },
-
-        /**
-         * Test Mailer Tasks
-         * ===============================
-         */
-        nodemailer: {
-            options: {
-                transport: {
-                    type: 'SMTP',
-                    options: {
-                        service: '<%= paths.sender.service  %>',
-                        auth: {
-                            user: '<%= paths.sender.user  %>',
-                            pass: '<%= paths.sender.pass  %>'
-                        }
-                    }
-                },
-                message: {
-                  subject: '<%= paths.recipients.subject  %>'
-                },
-                recipients: [
-              		{
-                		name: '<%= paths.recipients.name  %>',
-                		email: '<%= paths.recipients.email %>'
-              		}
-            		]
-            },
-            dist: {
-                src: ['<%= paths.dist %>/<%= paths.email %>/index.html']
-            }
-        },
-        
-        prompt: {
-            chooseFile: {
-              options: {
-                questions: [{
-                  config: 'paths.email',
-                  type: 'list',
-                  message: 'Which email should we build?',
-                  choices: choices
-                }]
-              }
-            },
-            target: {
-                options: {
-                    questions: [{
-                        config: 'paths.recipients.name',
-                        type: 'input',
-                        message: 'Who should we send this to (name)?',
-                        default: '<%= paths.recipients.name  %>'
-                    },
-                    {
-                        config: 'paths.recipients.email',
-                        type: 'input',
-                        message: 'Who should we send this to (email)?',
-                        default: '<%= paths.recipients.email  %>'
-                    },
-                    {
-                        config: 'paths.recipients.subject',
-                        type: 'input',
-                        message: 'What is the subject?',
-                        default: '<%= paths.recipients.subject  %>'
-                    }]
-                }
-            }
+    /**
+     * SCSS Compilation Tasks
+     * ===============================
+     */
+    compass: {
+      options: {
+        sassDir: '<%= paths.src %>/<%= paths.directory %>',
+        httpImagesPath: '<%= paths.src %>/<%= paths.images %>'
+      },
+      dev: {
+        options: {
+          cssDir: '<%= paths.tmp %>/<%= paths.directory %>/css',
+          imagesDir: '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.images %>',
+          outputStyle: 'expanded',
+          noLineComments: false
         }
-
-    });
-
-    grunt.registerTask('default', 'serve');
-
-    grunt.registerTask('serve', function (target) {
-        
-      if (target === 'dist') {
-        return grunt.task.run(['build', 'connect:dist']);
+      },
+      dist: {
+        options: {
+          force: true,
+          cssDir: '<%= paths.dist %>/<%= paths.directory %>/css',
+          imagesDir: '<%= paths.dist %>/<%= paths.directory %>/<%= paths.images %>',
+          noLineComments: true,
+          assetCacheBuster: false,
+          outputStyle: "compact"
+        }
       }
-  
-      grunt.task.run([
-        'chooseFile',
-        'compass:dev',
-        'connect:dev',
-        'watch'
-      ]);
-    });
-    
-    
-    grunt.registerTask('chooseFile', function (target) {
-        
-      if (templates.length > 1) {
-        return grunt.task.run(['prompt:chooseFile']);
-      }
-    });
-    
-    grunt.registerTask('build', [
-        'chooseFile',
-        'clean',
-        'mkdir',
-        'img_find_and_copy',
-        'imagemin',
-        'compass:dist',
-        'premailer:dist',
-        'premailer:plain',
-        'replace'
-    ]);
+    },
 
-    grunt.registerTask('send', [
-        'build',
-        'prompt:target',
-        'nodemailer'
+    /**
+     * Jade Compilation Tasks
+     * ===============================
+     */
+    jade: {
+      dist: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= paths.src %>',
+          src: ['<%= paths.directory %>/**/*.jade'],
+          dest: '<%= paths.tmp %>',
+          ext: '.html'
+        }]
+      }
+    },
+
+    /**
+     * Watch Task
+     * ===============================
+     */
+    watch: {
+      compass: {
+        files: ['<%= paths.src %>/scss/**/*.scss'],
+        tasks: ['compass:dev']
+      },
+      jade: {
+        options: {
+          spawn: false,
+          livereload: true
+        },
+        files: [
+          '<%= paths.src %>/<%= paths.directory %>/<%= paths.file %>.jade',
+          '<%= paths.src %>/_components/**/*.jade'
+        ],
+        tasks: ['jade']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>',
+        },
+        files: [
+          '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.file %>.html',
+          '<%= paths.tmp %>/<%= paths.directory %>/css/{,*/}*.css',
+          '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.images %>/{,*/}*.{png,jpg,jpeg,gif}'
+        ]
+      }
+    },
+
+    /**
+     * Server Tasks
+     * ===============================
+     */
+    connect: {
+      options: {
+        open: true,
+        hostname: 'localhost',
+        port: 8000,
+        livereload: 35729
+      },
+      dev: {
+        options: {
+          base: '<%= paths.tmp %>/<%= paths.directory %>',
+          open: {
+            target: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>/<%= paths.file %>.html'
+          }
+        }
+      },
+      dist: {
+        options: {
+          keepalive: true,
+          livereload: false,
+          base: '<%= paths.dist %>/<%= paths.directory %>'
+        }
+      }
+    },
+
+    /**
+     * Cleanup Tasks
+     * ===============================
+     */
+    clean: {
+      dev: ['<%= paths.tmp %>'],
+      dist: ['<%= paths.dist %>/<%= paths.directory %>']
+    },
+    mkdir: {
+      dist: {
+        options: {
+          create: ['<%= paths.tmp %>']
+        },
+      },
+    },
+
+    /**
+     * Images Optimization Tasks
+     * ===============================
+     */
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= paths.tmp %>',
+          src: '<%= paths.directory %>/<%= paths.images %>/{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= paths.dist %>'
+        }]
+      }
+    },
+
+    /**
+     * Premailer Parser Tasks
+     * ===============================
+     */
+    premailer: {
+      options: {
+        baseUrl: '<%= paths.distDomain %>'
+      },
+      plain: {
+        options: {
+          mode: 'txt'
+        },
+        src: '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.file %>.html',
+        dest: '<%= paths.dist %>/<%= paths.directory %>/index.txt'
+      },
+      dist: {
+        src: '<%= paths.tmp %>/<%= paths.directory %>/<%= paths.file %>.html',
+        dest: '<%= paths.dist %>/<%= paths.directory %>/index.html'
+      }
+    },
+
+    /**
+     * Replacement Tasks
+     * ===============================
+     */
+    replace: {
+      imageDir: {
+        src: '<%= paths.dist %>/<%= paths.directory %>/index.html',
+        dest: '<%= paths.dist %>/<%= paths.directory %>/index.html',
+        replacements: [{
+          from: '/<%= paths.images %>/',
+          to: '/'
+        }, {
+          from: 'margin',
+          to: 'Margin'
+        }]
+      }
+    },
+
+    /**
+     * Test Mailer Tasks
+     * ===============================
+     */
+    nodemailer: {
+      options: {
+        transport: {
+          type: 'SMTP',
+          options: {
+            service: '<%= paths.sender.service  %>',
+            auth: {
+              user: '<%= paths.sender.user  %>',
+              pass: '<%= paths.sender.pass  %>'
+            }
+          }
+        },
+        message: {
+          subject: '<%= paths.recipients.subject  %>'
+        },
+        recipients: [{
+          name: '<%= paths.recipients.name  %>',
+          email: '<%= paths.recipients.email %>'
+        }]
+      },
+      dist: {
+        src: ['<%= paths.dist %>/<%= paths.directory %>/index.html']
+      }
+    },
+
+    prompt: {
+      chooseFile: {
+        options: {
+          questions: [{
+            config: 'paths.file',
+            type: 'list',
+            message: 'Which email should we build?',
+            choices: choices,
+            filter: function(answer) {
+              grunt.config('paths.directory', answer.substr(0, answer.indexOf('/')));
+              return answer.substr(answer.indexOf('/') + 1)
+            }
+          }]
+        }
+      },
+      target: {
+        options: {
+          questions: [{
+            config: 'paths.recipients.name',
+            type: 'input',
+            message: 'Who should we send this to (name)?',
+            default: '<%= paths.recipients.name  %>'
+          }, {
+            config: 'paths.recipients.email',
+            type: 'input',
+            message: 'Who should we send this to (email)?',
+            default: '<%= paths.recipients.email  %>'
+          }, {
+            config: 'paths.recipients.subject',
+            type: 'input',
+            message: 'What is the subject?',
+            default: '<%= paths.recipients.subject  %>'
+          }]
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', 'serve');
+
+  grunt.registerTask('serve', function(target) {
+
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist']);
+    }
+
+    grunt.task.run([
+      'chooseFile',
+      'clean:dev',
+      'copy',
+      'compass:dev',
+      'jade',
+      'connect:dev',
+      'watch'
     ]);
+  });
+
+
+  grunt.registerTask('chooseFile', function(target) {
+
+    if (templates.length > 1) {
+      return grunt.task.run(['prompt:chooseFile']);
+    }
+  });
+
+  grunt.registerTask('build', [
+    'chooseFile',
+    'clean:dist',
+    'mkdir',
+    'img_find_and_copy',
+    'compass:dist',
+    'jade',
+    'imagemin',
+    'premailer:dist',
+    'premailer:plain',
+    'replace'
+  ]);
+
+  grunt.registerTask('send', [
+    'build',
+    'prompt:target',
+    'nodemailer'
+  ]);
 
 };
